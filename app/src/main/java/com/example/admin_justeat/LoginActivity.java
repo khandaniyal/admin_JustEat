4f1bee75e67bd11844f16b0cf711373d4e91f343
martingrub <eric27san@gmail.com>
2021-12-02 16:17:38 +0100

    Acabado el login y el logout con google

--- app/src/main/java/com/example/admin_justeat/LoginActivity.java
+++ app/src/main/java/com/example/admin_justeat/LoginActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -68,7 +69,8 @@ public class LoginActivity extends AppCompatActivity {
 
         Button loginButton = findViewById(R.id.register_button);
         Button registerButton = findViewById(R.id.login_register);
-        Button GoogleButton = findViewById(R.id.loginGoogle);
+
+        SignInButton signInButton = findViewById(R.id.google_login_button);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        GoogleButton.setOnClickListener(new View.OnClickListener() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Alert();
